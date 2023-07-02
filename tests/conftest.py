908d7ad22f4602cb454a970531cbe0097120e364
Olga-koml <olyakomleva@mail.ru>
2023-07-02 08:57:53 +0300

    added ingredients_test, edited fixtures & conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 import sys
 from os.path import abspath, dirname, join
+import pytest
+from django.conf import settings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
@@ -7,5 +13,38 @@ infra_dir_path = join(root_dir, 'infra')
 
 pytest_plugins = [
     'tests.fixtures.fixture_user',
-    #'tests.fixtures.fixture_data',
+    'tests.fixtures.fixture_data',
 ]
+
+
+# import pytest
+
+# @pytest.mark.django_db
+# def test_my_user():
+#     me = User.objects.get(username='me')
+#     assert me.is_superuser
+
+
+
+@pytest.fixture(scope='session')
+def django_db_setup():
+    settings.DATABASES['default'] = {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'HOST': os.getenv('DB_PORT', default='5432'),
+        'NAME': os.getenv('POSTGRES_DB', default='foodgram'),
+        'USER': os.getenv('POSTGRES_USER', default='foodgram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='1234'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
+        'ATOMIC_REQUESTS': True,
+    }
+
+# from django.core.management import call_command
+
+
+
+
+# @pytest.fixture(scope='session')
+# def django_db_setup(django_db_setup, django_db_blocker):
+#     with django_db_blocker.unblock():
+#         call_command('loaddata', 'my_fixture.json')
+
--- tests/fixtures/fixture_user.py
+++ tests/fixtures/fixture_user.py
@@ -22,22 +22,36 @@ def user(django_user_model):
 #    )
 #
 #
-#@pytest.fixture
-#def token(user):
+# @pytest.fixture
+# def token(user):
 #    from rest_framework_simplejwt.tokens import RefreshToken
 #    refresh = RefreshToken.for_user(user)
-#
+
 #    return {
 #        'refresh': str(refresh),
 #        'access': str(refresh.access_token),
 #    }
-#
-#
-#@pytest.fixture
-#def user_client(token):
-#    from rest_framework.test import APIClient
-#
-#    client = APIClient()
-#    client.credentials(HTTP_AUTHORIZATION=f'Bearer {token["access"]}')
-#    return client
-#
+
+
+# @pytest.fixture
+# def user_client(token):
+#     from rest_framework.test import APIClient
+
+#     client = APIClient()
+#     client.credentials(HTTP_AUTHORIZATION=f'Bearer {token["access"]}')
+#     return client
+
+@pytest.fixture
+def token(user):
+    from rest_framework.authtoken.models import Token
+    token, _ = Token.objects.get_or_create(user=user)
+    return token.key
+
+
+@pytest.fixture
+def user_client(token):
+    from rest_framework.test import APIClient
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+    return client
